207bd165dc11a15deb95be7d0a4fa26cb47a91ed
phantranfe <phantran.fe@gmail.com>
2026-02-24 09:20:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -172,7 +172,7 @@ io.on('connection', (socket) => {
     socket.on('claim_win', ({roomId, userName}) => {
         const room = rooms[roomId];
         if (room) {
-            io.to(roomId).emit('win', `${userName} đã thắng, hãy cùng nhau kiểm tra lại.`);
+            io.to(roomId).emit('win', userName);
         }
     });
 
